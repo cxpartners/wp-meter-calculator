8d2814b0114f0db4a8427fe15f1fb30b2823a856
Henry <henry_ball@hotmail.co.uk>
2019-07-25 15:21:21 +0100

    update build paths

--- wp-micro-frontend.php
+++ wp-micro-frontend.php
@@ -5,9 +5,10 @@
 
   function wpmicrofe_shortcodes_init() {  
     add_shortcode('wpmicrofe', function() {
-      wp_enqueue_script('reactAppRuntime', plugins_url('/js/build/static/js/runtime~main.c87cc1eb.js', __FILE__)); 
-      wp_enqueue_script('reactApp', plugins_url('/js/build/static/js/2.c272f57a.chunk.js', __FILE__));
-      wp_enqueue_script('reactAppMain', plugins_url('/js/build/static/js/main.0e49e05e.chunk.js', __FILE__)); 
+    	wp_enqueue_style('meter-calculator-css', plugins_url('/bristol-water-calculator/build/static/css/main.87f97069.chunk.css', __FILE__));
+      wp_enqueue_script('reactAppRuntime', plugins_url('/bristol-water-calculator/build/static/js/runtime~main.a8a9905a.js', __FILE__)); 
+      wp_enqueue_script('reactApp', plugins_url('/bristol-water-calculator/build/static/js/2.4b40e7a7.chunk.js', __FILE__));
+      wp_enqueue_script('reactAppMain', plugins_url('/bristol-water-calculator/build/static/js/main.474da2e3.chunk.js', __FILE__)); 
       return sprintf("<div id=\"root\"></div>");
     });
   }
